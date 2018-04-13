a4a15486c5e3890fa1e8db500437abbf0f78ee80
Akio Taniguchi <taniguchi@ioa.s.u-tokyo.ac.jp>
2018-04-13 11:08:33 +0900

    :+1: Update class

--- dpop/db.py
+++ dpop/db.py
@@ -10,22 +10,27 @@ from collections import OrderedDict
 
 # dependent packages
 import yaml
+import numpy as np
+from astropy import constants
 from astropy import units as u
 
 # module constants
-config = yaml.load(get_data('dpop', 'data/config.yaml'))
+CONFIG = yaml.load(get_data('dpop', 'data/config.yaml'))
+c = constants.c
+h = constants.h
+k = constants.k_B
 
 
 # classes
 class LAMDA(object):
-    avalilable = list(config['lamda_mol'].keys())
+    available = list(CONFIG['lamda_mol'].keys())
 
     def __init__(self, molname, *, encoding='utf-8'):
-        mol = config['lamda_mol'][molname]
-        url = config['lamda_url'] + f'/{mol}'
+        mol = CONFIG['lamda_mol'][molname]
         path = Path(mol)
 
         if not path.exists():
+            url = CONFIG['lamda_url'] + '/' + mol
             with urlopen(url) as data, path.open('w') as f:
                 f.write(data.read().decode(encoding))
 
@@ -34,50 +39,79 @@ class LAMDA(object):
             self.energy_levels = self._get_energy_levels(f)
             self.transitions = self._get_transitions(f)
 
-        self.available = list(self.transitions.keys())
+    def Z(self, T_ex):
+        if not isinstance(T_ex, u.Quantity):
+            T_ex = T_ex * u.K
+
+        Z = 0.0
+        for val in self.energy_levels.values():
+            g = val['weight']
+            E = h * c * val['energy']
+            Z += g * np.exp(-E/(k*T_ex))
+
+        return Z.to(u.one)
+
+    def dZdT(self, T_ex):
+        if not isinstance(T_ex, u.Quantity):
+            T_ex = T_ex * u.K
+
+        dZdT = 0.0
+        for val in self.energy_levels.values():
+            g = val['weight']
+            E = h * c * val['energy']
+            dZdT += g * E/(k*T_ex**2) * np.exp(-E/(k*T_ex))
+
+        return dZdT.to(u.K**-1)
 
     def _get_energy_levels(self, f):
-        kwd = ''
-        pat = re.compile('energy levels', re.IGNORECASE)
-        while not pat.search(kwd):
-            kwd = f.readline()
+        energy_levels = OrderedDict()
+        self._skip_lines(f, 'ENERGY LEVELS$')
 
         n_levels = int(f.readline())
-        f.readline()
-
-        energy_levels = OrderedDict()
+        self._skip_lines(f, '^!LEVEL')
 
         for i in range(n_levels):
             elems  = f.readline().split()
-            energy = float(elems[1]) / u.cm
-            weight = float(elems[2]) / u.dimensionless_unscaled
-            level  = elems[3]
+            energy = float(elems[1]) * u.cm**-1
+            weight = float(elems[2]) * u.one
 
-            energy_levels[level] = {'energy': energy, 'weight': weight}
+            key = elems[3]
+            val = {'energy': energy, 'weight': weight}
+            energy_levels[key] = val
 
         return energy_levels
 
     def _get_transitions(self, f):
-        kwd = ''
-        pat = re.compile('radiative transitions', re.IGNORECASE)
-        while not pat.search(kwd):
-            kwd = f.readline()
-
-        n_transitions = int(f.readline())
-        f.readline()
-
         transitions = OrderedDict()
         levels = list(self.energy_levels.keys())
+        self._skip_lines(f, 'RADIATIVE TRANSITIONS$')
+
+        n_transitions = int(f.readline())
+        self._skip_lines(f, '^!TRANS')
 
         for i in range(n_transitions):
             elems  = f.readline().split()
             upper  = levels[int(elems[1])-1]
             lower  = levels[int(elems[2])-1]
-            A_ul   = float(elems[3]) / u.s
+            A_ul   = float(elems[3]) * u.s**-1
             f_rest = float(elems[4]) * u.GHz
             E_u    = float(elems[5]) * u.K
 
-            name = f'{upper}-{lower}'
-            transitions[name] = {'A_ul': A_ul, 'f_rest': f_rest, 'E_u': E_u}
+            key = (upper, lower)
+            val = {'A_ul': A_ul, 'f_rest': f_rest, 'E_u': E_u}
+            transitions[key] = val
 
         return transitions
+
+    @staticmethod
+    def _skip_lines(f, pattern):
+        pattern = re.compile(pattern)
+
+        line = ''
+        while not pattern.search(line):
+            line = f.readline()
+
+        return f
+
+    def __repr__(self):
+        return f'LAMDA({self.molname})'
